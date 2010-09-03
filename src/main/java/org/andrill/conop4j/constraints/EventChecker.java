ce34b6d6d053aeca43e854e738eedf8e88cdc856
Josh Reed <jareed@andrill.org>
2010-09-02 22:07:35 -0700

    Removed spurious print statements

--- src/main/java/org/andrill/conop4j/constraints/EventChecker.java
+++ src/main/java/org/andrill/conop4j/constraints/EventChecker.java
@@ -16,12 +16,10 @@ public class EventChecker implements ConstraintChecker {
 			int position = solution.getPosition(e);
 			Event before = e.getBeforeConstraint();
 			if ((before != null) && (position > solution.getPosition(before))) {
-				System.err.println(e.getName() + " should be before " + before.getName());
 				return false;
 			}
 			Event after = e.getAfterConstraint();
 			if ((after != null) && (position < solution.getPosition(after))) {
-				System.err.println(e.getName() + " should be after " + after.getName());
 				return false;
 			}
 		}
