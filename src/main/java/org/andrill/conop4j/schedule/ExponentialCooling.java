47b32d8881dacab025b17a593cfc6f3747b46166
Josh Reed <jareed@andrill.org>
2010-10-04 09:50:12 -0500

    Updated the ExponentialCooling schedule to allow multiple steps per temperature

--- src/main/java/org/andrill/conop4j/schedule/ExponentialCooling.java
+++ src/main/java/org/andrill/conop4j/schedule/ExponentialCooling.java
@@ -11,6 +11,8 @@ public class ExponentialCooling implements CoolingSchedule {
 	protected double current = 0;
 	protected double factor = 0.001;
 	protected double initial = 0;
+	protected long stepsLeft = 1;
+	protected long stepsPer = 1;
 
 	/**
 	 * Create a new exponential cooling schedule.
@@ -24,6 +26,26 @@ public class ExponentialCooling implements CoolingSchedule {
 		this.initial = initial;
 		this.factor = factor;
 		current = initial;
+		stepsPer = 1;
+		stepsLeft = stepsPer;
+	}
+
+	/**
+	 * Create a new exponential cooling schedule.
+	 * 
+	 * @param initial
+	 *            the initial temperature.
+	 * @param factor
+	 *            the cooling factor (< 0.01)
+	 * @param stepsPer
+	 *            the steps per temperature.
+	 */
+	public ExponentialCooling(final double initial, final double factor, final long stepsPer) {
+		this.initial = initial;
+		this.factor = factor;
+		this.stepsPer = stepsPer;
+		current = initial;
+		stepsLeft = stepsPer;
 	}
 
 	@Override
@@ -33,9 +55,13 @@ public class ExponentialCooling implements CoolingSchedule {
 
 	@Override
 	public double next(final Solution solution) {
-		current = current / (1 + current * factor);
-		if (current < 0.01) {
-			current = 0;
+		stepsLeft--;
+		if (stepsLeft == 0) {
+			current = current / (1 + current * factor);
+			if (current < 0.01) {
+				current = 0;
+			}
+			stepsLeft = stepsPer;
 		}
 		return current;
 	}
