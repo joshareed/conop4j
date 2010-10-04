9125e0a43ebe2362a6285088f626011b598a3275
Josh Reed <jareed@andrill.org>
2010-10-04 09:52:46 -0500

    Updated the ConstraintsScore to use the CoexistenceMatrix class

--- src/main/java/org/andrill/conop4j/scoring/ConstraintsScore.java
+++ src/main/java/org/andrill/conop4j/scoring/ConstraintsScore.java
@@ -1,10 +1,14 @@
 package org.andrill.conop4j.scoring;
 
+import java.util.Map;
+
 import org.andrill.conop4j.CoexistenceMatrix;
 import org.andrill.conop4j.Event;
 import org.andrill.conop4j.Run;
 import org.andrill.conop4j.Section;
 import org.andrill.conop4j.Solution;
+
+import com.google.common.collect.Maps;
 
 /**
  * Calculates a score based on the number of observed coexistences the potential
@@ -13,10 +17,21 @@ import org.andrill.conop4j.Solution;
  * @author Josh Reed (jareed@andrill.org)
  */
 public class ConstraintsScore implements ScoringFunction {
-
 	@Override
 	public Type getType() {
 		return Type.SCORE;
+	}
+
+	protected void remove(final Event e, final Map<Event, Boolean> check) {
+		check.put(e, true);
+		Event before = e.getBeforeConstraint();
+		if (before != null) {
+			check.put(before, true);
+		}
+		Event after = e.getAfterConstraint();
+		if (after != null) {
+			check.put(after, true);
+		}
 	}
 
 	@Override
@@ -24,13 +39,18 @@ public class ConstraintsScore implements ScoringFunction {
 		double score = 0;
 		Run run = solution.getRun();
 		CoexistenceMatrix sm = new CoexistenceMatrix(solution);
-		for (Event e1 : solution.getEvents()) {
-			for (Event e2 : solution.getEvents()) {
-				for (Section s : run.getSections()) {
-					CoexistenceMatrix cm = run.getCoexistenceMatrix(s);
-					if (sm.getCoexistence(e1, e2) == cm.getCoexistence(e1, e2)) {
-						score += 1;
+		for (Section s : run.getSections()) {
+			CoexistenceMatrix cm = run.getCoexistenceMatrix(s);
+			Map<Event, Boolean> check = Maps.newHashMap();
+			for (Event e1 : s.getEvents()) {
+				remove(e1, check);
+				for (Event e2 : s.getEvents()) {
+					if (!check.containsKey(e2)) {
+						if (sm.getCoexistence(e1, e2) == cm.getCoexistence(e1, e2)) {
+							score += 1;
+						}
 					}
+					remove(e2, check);
 				}
 			}
 		}
