d86809af38c7f5fbb6a2d7befdc50e3231cedad8
Josh Reed <jareed@andrill.org>
2011-01-04 17:18:23 -0600

    Added a ConstraintChecker and ObjectiveFunction based on coexistences.  Both are experimental and need more work.

---
+++ src/main/java/org/andrill/conop4j/objective/CoexistencePenalty.java
@@ -0,0 +1,25 @@
+package org.andrill.conop4j.objective;
+
+import org.andrill.conop4j.CoexistenceMatrix;
+import org.andrill.conop4j.CoexistenceMatrix.Coexistence;
+import org.andrill.conop4j.Event;
+import org.andrill.conop4j.Solution;
+
+public class CoexistencePenalty implements ObjectiveFunction {
+
+	@Override
+	public double score(final Solution solution) {
+		int violations = 0;
+		CoexistenceMatrix rm = solution.getRun().getCoexistenceMatrix();
+		CoexistenceMatrix sm = new CoexistenceMatrix(solution);
+		for (Event e1 : solution.getEvents()) {
+			for (Event e2 : solution.getEvents()) {
+				Coexistence rc = rm.getCoexistence(e1, e2);
+				if ((rc != Coexistence.ABSENT) && (rc != Coexistence.MIXED) && (rc != sm.getCoexistence(e1, e2))) {
+					violations++;
+				}
+			}
+		}
+		return violations;
+	}
+}
