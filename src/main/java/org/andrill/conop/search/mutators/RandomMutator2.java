f9de9d20104fa624233fd595b5a4dd4dc1a60ad8
Josh Reed <jareed@andrill.org>
2011-03-29 09:15:30 -0500

    Tweaked the RandomMutator2

--- src/main/java/org/andrill/conop/search/mutators/RandomMutator2.java
+++ src/main/java/org/andrill/conop/search/mutators/RandomMutator2.java
@@ -9,25 +9,43 @@ import org.andrill.conop.search.Solution;
 import com.google.common.collect.Lists;
 
 public class RandomMutator2 extends AbstractMutator {
+	protected List<Event> first;
+	protected int ptr = -1;
 	protected Random random = new Random();
+	protected double temp;
 
 	@Override
 	protected Solution internalMutate(final Solution solution) {
+		if (ptr == -1) {
+			ptr = 0;
+			first = solution.getEvents();
+		}
+
+		// get our events
 		List<Event> events = Lists.newArrayList(solution.getEvents());
 
-		// pick a random event and move it to a new position randomly
-		int cur = random.nextInt(events.size());
-		int range = ((int) Math.log(temp) + 1) * 2;
+		// remove our event
+		int i = events.indexOf(first.get(ptr));
+		Event e = events.remove(i);
+
+		// figure out our range
+		int range = (int) Math.ceil(temp / 10) + 1;
 		int diff = random.nextInt(2 * range) - range;
 
 		// build a new solution
-		Event e = events.remove(cur);
-		events.add(Math.min(events.size(), Math.max(0, cur + diff)), e);
+		events.add(Math.min(events.size(), Math.max(0, i + diff)), e);
 		return new Solution(solution.getRun(), events);
 	}
 
 	@Override
 	public String toString() {
 		return "Random2";
+	}
+
+	@Override
+	public void tried(final double temp, final Solution current, final Solution best) {
+		super.tried(temp, current, best);
+		this.temp = temp;
+		ptr = (ptr + 1) % first.size();
 	}
 }
