054d2bdc8256029cc4a1b188b965bf59d92abd1f
Josh Reed <jareed@andrill.org>
2013-10-01 21:26:01 -0500

    Templatize strings

--- src/main/java/org/andrill/conop/search/listeners/ConsoleProgressListener.java
+++ src/main/java/org/andrill/conop/search/listeners/ConsoleProgressListener.java
@@ -10,6 +10,8 @@ import org.andrill.conop.search.Solution;
  * @author Josh Reed (jareed@andrill.org)
  */
 public class ConsoleProgressListener extends AsyncListener {
+	private static final String CLEAR = "                                                                                    \r";
+	private static final String FORMAT = "CONOP4J: %s / %s [ %d min | %s C | %d ] (%d/s)\r";
 	private static final DecimalFormat DEC = new DecimalFormat("0.00");
 	private double score = Double.MAX_VALUE;
 
@@ -22,11 +24,9 @@ public class ConsoleProgressListener extends AsyncListener {
 	protected void run(final double temp, final long iteration, final Solution current, final Solution best) {
 		score = current.getScore();
 		long elapsed = (System.currentTimeMillis() - start);
-		System.out.print("                                                                            \r");
-		System.out.print("CONOP4J: " + DEC.format(score) + "/" + DEC.format(best.getScore()) + " [ "
-				+ (elapsed / 60000)
-				+ "min | " + DEC.format(temp)
-				+ "C | " + iteration + " ] (" + (iteration / (elapsed / 1000)) + "/s)\r");
+		System.out.print(CLEAR);
+		System.out.print(String.format(FORMAT, DEC.format(score), DEC.format(best.getScore()), (elapsed / 60000),
+				DEC.format(temp), iteration, (iteration / (elapsed / 1000))));
 	}
 
 	@Override
