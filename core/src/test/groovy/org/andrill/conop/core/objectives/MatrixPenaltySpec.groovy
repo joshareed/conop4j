a610cc2601c1c74d08108e8cefd03b04d9f02fe0
Josh Reed <jareed@andrill.org>
2014-05-28 19:58:08 -0500

    Add tests for the existing objective functions

---
+++ core/src/test/groovy/org/andrill/conop/core/objectives/MatrixPenaltySpec.groovy
@@ -0,0 +1,30 @@
+package org.andrill.conop.core.objectives
+
+import org.andrill.conop.core.Simulation
+import org.andrill.conop.core.RunFixtures
+
+import spock.lang.Specification
+
+class MatrixPenaltySpec extends Specification {
+
+	def "scoring a simple run"() {
+		given: 'a run'
+		def run = RunFixtures.simpleRun()
+
+		and: 'a simulation'
+		def simulation = new Simulation(new Properties(), run)
+
+		and: 'the placement penalty'
+		def objective = new MatrixPenalty()
+		objective.configure(simulation)
+
+		and: 'a possible solution'
+		def initial = RunFixtures.simpleRunBest(run)
+
+		when: 'score the solution'
+		def score = objective.score(initial)
+
+		then: 'the score is zero'
+		score == 0
+	}
+}
