b3119bb8d9ddd6da912533ab85f6c223d5e458af
Josh Reed <jareed@andrill.org>
2014-06-04 22:44:55 -0500

    Move test local repository around

--- data/src/test/groovy/org/andrill/conop/data/LocalRepositorySpec.groovy
+++ data/src/test/groovy/org/andrill/conop/data/LocalRepositorySpec.groovy
@@ -6,11 +6,11 @@ class LocalRepositorySpec extends Specification {
 
 	def "get all locations"() {
 		given: 'a local repository'
-		def repo = new LocalRepository('src/test/resources/repo')
-		
+		def repo = new LocalRepository('src/test/resources/repos/local')
+
 		when: 'get locations'
 		def locations = repo.locations
-		
+
 		then: 'found all locations'
 		assert locations
 		assert 1 == locations.size()
@@ -19,11 +19,11 @@ class LocalRepositorySpec extends Specification {
 
 	def "get a location"() {
 		given: 'a local repository'
-		def repo = new LocalRepository('src/test/resources/repo')
-		
+		def repo = new LocalRepository('src/test/resources/repos/local')
+
 		when: 'get and1-1b'
 		def location = repo.getLocation("and1-1b")
-		
+
 		then: 'found and1-1b'
 		assert location
 		assert 'and1-1b' == location.id
@@ -31,30 +31,29 @@ class LocalRepositorySpec extends Specification {
 
 	def "get all events"() {
 		given: 'a local repository'
-		def repo = new LocalRepository('src/test/resources/repo')
-		
+		def repo = new LocalRepository('src/test/resources/repos/local')
+
 		when: 'get events'
 		def events = repo.events
-		
+
 		then: 'found all events'
 		assert events
 		assert 2 == events.size()
 		assert events.find { it.id == 'fragilariopsis-curta' }
 		assert events.find { it.id == 'fragilariopsis-fossilis' }
 	}
-	
+
 	def "get observations"() {
 		given: 'a local repository'
-		def repo = new LocalRepository('src/test/resources/repo')
-		
+		def repo = new LocalRepository('src/test/resources/repos/local')
+
 		when: 'get observations'
 		def observations = repo.getObservations("and1-1b")
-		
+
 		then: 'found all observations'
 		assert observations
 		assert 2 == observations.size()
 		assert observations.find { it.event.id == 'fragilariopsis-curta' }
 		assert observations.find { it.event.id == 'fragilariopsis-fossilis' }
-		
 	}
 }
