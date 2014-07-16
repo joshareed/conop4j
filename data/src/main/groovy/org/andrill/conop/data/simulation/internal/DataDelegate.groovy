fb0195272a55e2995f1a8c7931232fad3b768f74
Josh Reed <jareed@andrill.org>
2014-07-15 22:46:09 -0500

    Use ‘location’ instead of ‘include’
    Planning on using ‘include’ to whitelist events

--- data/src/main/groovy/org/andrill/conop/data/simulation/internal/DataDelegate.groovy
+++ data/src/main/groovy/org/andrill/conop/data/simulation/internal/DataDelegate.groovy
@@ -3,7 +3,11 @@ package org.andrill.conop.data.simulation.internal
 class DataDelegate {
 	def locations = []
 
-	def include(String id) {
+	def location(Map params) {
+		locations << "${params.program}:${params.id}"
+	}
+
+	def location(String id) {
 		locations << id
 	}
 }
