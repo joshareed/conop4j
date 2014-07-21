8355c1c9e1ee7171c6618cb4f5981800d9bd9c63
Josh Reed <jareed@andrill.org>
2014-07-21 07:28:16 -0500

    Add variable weights to the observations

--- data/src/main/groovy/org/andrill/conop/data/internal/AbstractJsonRepository.groovy
+++ data/src/main/groovy/org/andrill/conop/data/internal/AbstractJsonRepository.groovy
@@ -2,6 +2,7 @@ package org.andrill.conop.data.internal
 
 import groovy.json.JsonSlurper
 
+import org.andrill.conop.core.Event
 import org.andrill.conop.core.Location
 import org.andrill.conop.core.internal.DefaultEvent
 import org.andrill.conop.core.internal.DefaultLocation
@@ -32,9 +33,31 @@ abstract class AbstractJsonRepository implements Repository {
 		def observations = []
 		json.observations.each { o ->
 			def event = new DefaultEvent(o.event.name)
-			observations << new DefaultObservation(event, o.level, 1, 1)
+			def up = getWeightUp(event)
+			def dn = getWeightDown(event)
+			observations << new DefaultObservation(event, o.level, up, dn)
 		}
 
 		return new DefaultLocation(json.name, observations)
+	}
+
+	protected double getWeightUp(Event e) {
+		if (e.name.endsWith("AGE")) {
+			return 100000
+		} else if (e.name.endsWith("FAD")) {
+			return 100000
+		} else {
+			return 1
+		}
+	}
+
+	protected double getWeightDown(Event e) {
+		if (e.name.endsWith("AGE")) {
+			return 100000
+		} else if (e.name.endsWith("LAD")) {
+			return 100000
+		} else {
+			return 1
+		}
 	}
 }
