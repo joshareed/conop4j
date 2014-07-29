bacc2eebe0cadc0de12ec0c6830481d1491a0408
Josh Reed <jareed@andrill.org>
2014-07-29 16:33:19 -0500

    Clean up handler definitions

--- tracker/src/ratpack/Ratpack.groovy
+++ tracker/src/ratpack/Ratpack.groovy
@@ -3,7 +3,6 @@ import static ratpack.groovy.Groovy.ratpack
 import static ratpack.jackson.Jackson.json
 import io.conop.JobService
 import io.conop.TrackerModule
-import ratpack.form.Form
 import ratpack.jackson.JacksonModule
 
 ratpack {
@@ -20,64 +19,47 @@ ratpack {
 			render groovyTemplate("index.html")
 		}
 
-		prefix("api") {
-			prefix("jobs") {
-				prefix(":id") {
-					// job-specific handler
-					handler {
-						byMethod {
-							get {
-								def job = service.get(pathTokens?.id)
-								if (job) {
-									render json(job)
-								} else {
-									clientError(404)
-								}
-							}
-							post {
-								try {
-									def job = service.update(pathTokens?.id, request.body)
-									if (job) {
-										render json(job)
-									} else {
-										clientError(404)
-									}
-								} catch (e) {
-									e.printStackTrace()
-								}
-							}
-							delete {
-								def job = service.get(pathTokens?.id)
-								if (job) {
-									service.delete(job.id)
-									render "OK"
-								} else {
-									clientError(404)
-								}
-							}
-						}
+		handler("api/jobs") {
+			byMethod {
+				get {
+					render json(service.allJobs)
+				}
+				post {
+					def job = service.add(request.body.text)
+					job.url = "${launchConfig.publicAddress}/api/jobs/${job.id}".toString()
+
+					response.status 201
+					response.headers.set "Location", job.url
+					render json(job)
+				}
+			}
+		}
+
+		handler("api/jobs/:id") {
+			byMethod {
+				get {
+					def job = service.get(pathTokens?.id)
+					if (job) {
+						render json(job)
+					} else {
+						clientError 404
 					}
 				}
-
-				// job list handler
-				handler {
-					byMethod {
-						get {
-							render json(service.allJobs)
-						}
-						post {
-							Form form = parse(Form.class)
-							if (form.source) {
-								def job = service.add(form.source)
-								job.url = "${launchConfig.publicAddress}/api/jobs/${job.id}".toString()
-
-								response.status(201)
-								response.headers.set("Location", job.url)
-								render json(job)
-							} else {
-								clientError(400)
-							}
-						}
+				post {
+					def job = service.update(pathTokens?.id, request.body)
+					if (job) {
+						render json(job)
+					} else {
+						clientError 404
+					}
+				}
+				delete {
+					def job = service.get(pathTokens?.id)
+					if (job) {
+						service.delete(job.id)
+						clientError 204
+					} else {
+						clientError 404
 					}
 				}
 			}
