013a9506cd463e69bca88f35f4257b4d7a9ef1dc
Josh Reed <jareed@andrill.org>
2014-07-29 19:48:16 -0500

    Add very rudimentary persistence of jobs

--- tracker/src/main/groovy/io/conop/JobService.groovy
+++ tracker/src/main/groovy/io/conop/JobService.groovy
@@ -1,14 +1,46 @@
 package io.conop
 
+import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 import groovy.util.logging.Slf4j
 
 import java.security.SecureRandom
 
+import ratpack.launch.LaunchConfig
+
+import com.google.inject.Inject
+
 @Slf4j
 class JobService {
 	protected SecureRandom random = new SecureRandom()
 	protected jobs = []
+	protected File db = null
+
+	@Inject
+	JobService(LaunchConfig config) {
+		String path = config.getOther("jobs", null)
+		if (path) {
+			db = new File(path)
+			readJobs()
+		}
+	}
+
+	protected void readJobs() {
+		if (db == null) { return }
+
+		try {
+			def json = new JsonSlurper().parse(db)
+			jobs = json.collect { it }
+		} catch (e) {
+			log.error "Unable to parse ${db.absolutePath}", e
+		}
+	}
+
+	protected void writeJobs() {
+		if (db == null) { return }
+
+		db.write(JsonOutput.toJson(jobs))
+	}
 
 	Map add(String source) {
 		def job = [
@@ -18,10 +50,14 @@ class JobService {
 			stats: [
 				created: null,
 				updated: null,
-				iterations: 0
+				iterations: 0,
+				score: -1
 			]
 		]
 		jobs << job
+
+		writeJobs()
+
 		job
 	}
 
@@ -52,7 +88,10 @@ class JobService {
 			}
 			if (!job.solution || json.solution.score <= job.solution.score) {
 				job.solution = json.solution
+				job.stats.score = json.solution.score
 			}
+
+			writeJobs()
 
 			return job
 		} else {
@@ -64,6 +103,8 @@ class JobService {
 		jobs.findAll { it.id == id }.each { job ->
 			job.active = false
 		}
+
+		writeJobs()
 	}
 
 	protected String id() {
