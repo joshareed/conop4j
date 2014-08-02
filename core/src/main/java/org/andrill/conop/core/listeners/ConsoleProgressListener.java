e2f09038ddcf9a48ba9bc093ad732d277150660a
Josh Reed <jareed@andrill.org>
2014-08-02 11:12:36 -0500

    Ensure listener is only firing once at a time

--- core/src/main/java/org/andrill/conop/core/listeners/ConsoleProgressListener.java
+++ core/src/main/java/org/andrill/conop/core/listeners/ConsoleProgressListener.java
@@ -1,8 +1,10 @@
 package org.andrill.conop.core.listeners;
 
 import java.text.DecimalFormat;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.andrill.conop.core.Solution;
+import org.andrill.conop.core.util.TimerUtils;
 
 /**
  * A {@link Listener} that writes progress to the console.
@@ -11,24 +13,36 @@ import org.andrill.conop.core.Solution;
  */
 public class ConsoleProgressListener extends AsyncListener {
 	private static final String CLEAR = "                                                                                    \r";
-	private static final String FORMAT = "CONOP4J: %s / %s [ %d min | %s C | %d ] (%d/s)\r";
+	private static final String FORMAT = "CONOP4J: %s [ %d min | %s C | %d ] (%d/s)\r";
 	private static final DecimalFormat DEC = new DecimalFormat("0.00");
 	private double score = Double.MAX_VALUE;
+	private ReentrantLock lock = new ReentrantLock();
 
 	@Override
 	protected void run(final double temp, final long iteration,
 			final Solution current, final Solution best) {
-		score = best.getScore();
-		long elapsed = (System.currentTimeMillis() - start);
-		System.out.print(CLEAR);
-		System.out.print(String.format(FORMAT, DEC.format(score),
-				DEC.format(best.getScore()), (elapsed / 60000),
-				DEC.format(temp), iteration, (iteration / (elapsed / 1000))));
+		if (lock.tryLock()) {
+			try {
+				score = best.getScore();
+				long elapsed = TimerUtils.getCounter();
+				System.out.print(CLEAR);
+				System.out.print(String.format(FORMAT,
+						DEC.format(best.getScore()), (elapsed / 60),
+						DEC.format(temp), iteration, (iteration / elapsed)));
+			} finally {
+				lock.unlock();
+			}
+		}
 	}
 
 	@Override
 	protected boolean test(final double temp, final long iteration,
 			final Solution current, final Solution best) {
 		return (best.getScore() < score) || ((iteration % 10000) == 0);
+	}
+
+	@Override
+	public void stopped(Solution solution) {
+		System.out.print("\r\n");
 	}
 }
